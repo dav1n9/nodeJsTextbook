bf2d753eb9c1007dc6d6b65da0fff3526bbdd4cf
dav1n9 <davin9909@gmail.com>
2023-09-08 20:53:15 +0900

    feat: jwt 토큰으로 인증하기

--- nodebird-api/middlewares/index.js
+++ nodebird-api/middlewares/index.js
@@ -1,5 +1,6 @@
-// 로그인 여부를검사하는 미들웨어
+const jwt = require('jsonwebtoken');
 
+// 로그인 여부를검사하는 미들웨어
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {    // 로그인 중이면
         next();
@@ -14,5 +15,24 @@ exports.isNotLoggedIn = (req, res, next) => {
     } else {
         const message = encodeURIComponent('로그인한 상태입니다.');
         res.redirect(`/?error=${message}`);
+    }
+};
+
+// 토큰(jwt) 검증하는 미들웨어
+exports.verifyToken = (req, res, next) => {
+    try {
+        res.locals.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {   // 유효 기간 초과
+            return res.status(419).json({
+                code: 419,
+                message: '토큰이 만료되었습니다.',
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: '유효하지 않은 토큰입니다.',
+        });
     }
 };
