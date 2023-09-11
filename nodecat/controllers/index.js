4c17513a1b06de0d0957073193b9b8bddf7ad876
dav1n9 <davin9909@gmail.com>
2023-09-11 16:13:30 +0900

    feat: SNS API 사용하는 코드 추가
    토큰을 발급 받는 부분은 함수로 만들어 재사용 하였다.

--- nodecat/controllers/index.js
+++ nodecat/controllers/index.js
@@ -1,27 +1,47 @@
 const axios = require('axios');
 
-exports.test = async (req, res,next) => {
+const URL = process.env.API_URL;
+axios.defaults.headers.origin = process.env.ORIGIN;   // origin 헤더 추가
+
+const request = async (req, api) => {
     try {
         if (!req.session.jwt) { // 세션에 토큰이 없으면 토큰 발급 시도
-            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+            const tokenResult = await axios.post(`${URL}/token`, {
                clientSecret: process.env.CLIENT_SECRET, 
             });
-            if (tokenResult.data?.code === 200) {   // 토큰 발급 성공
-                req.session.jwt = tokenResult.data.token;   // 세션에 토큰 저장
-            } else {    // 토큰 발급 실패
-                return res.json(tokenResult.data);  // 발급 실패 사유 응답
-            }
+            req.session.jwt = tokenResult.data.token;   // 세션에 토큰 저장
         }
-        // 발급받은 토큰 테스트
-        const result = await axios.get('http://localhost:8002/v1/test', {
+        return await axios.get(`${URL}${api}`, {
             headers: { authorization: req.session.jwt },
-        });
-        return res.json(result.data);
+        }); // api 요청
+    } catch (error) {
+        if (error.response?.status === 419) {   // 토큰 만료시 토큰 재발급받기
+            delete req.session.jwt;
+            return this.request(req, api);
+        }   // 419 외의 다른 에러이면
+        return error.response;
+    }
+};
+
+exports.getMyPosts = async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
     } catch (error) {
         console.error(error);
-        if (error.response?.status === 419) {   // 토큰 만료시
-            return res.json(error.response.data);
+        next(error);
+    }
+};
+
+exports.searchByHashtag = async (req, res, next) => {
+    try {
+        // encodeURLComponent(req.params.hashtag)
+        const result = await request(req, `/posts/hashtag/${req.params.hashtag}`);
+        res.json(result.data);
+    } catch (error) {
+        if (error.code) {
+            console.error(error);
+            next(error);
         }
-        return next(error);
     }
 };
