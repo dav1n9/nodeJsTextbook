11fd9ff440e52a73fa7a29c356bfc5f075698efc
dav1n9 <davin9909@gmail.com>
2023-09-16 21:13:56 +0900

    models/index 주석 추가
    자동 initiate , associate 방법

--- nodebird/models/index.js
+++ nodebird/models/index.js
@@ -9,6 +9,7 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
+
 db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
@@ -20,5 +21,30 @@ Hashtag.initiate(sequelize);
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
+
+
+/*
+const basename = path.basename(__filename);
+fs
+  .readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
+  .filter(file => { // 숨김 파일, index.js, js 확장자가 아닌 파일 필터링
+    return (file.indexOf('.') !== 0) && !file.includes('test') && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => { // 해당 파일의 모델 불러와서 init
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
+    db[model.name] = model;
+    model.initiate(sequelize);
+  });
+
+Object.keys(db).forEach(modelName => { // associate 호출
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+*/
+
+
 
 module.exports = db;
